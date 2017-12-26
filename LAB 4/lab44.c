e92d67abd64481f7e2ccb18a95fc293cd23ac544
Maverick <rabin.adk1@gmail.com>
2017-12-26 20:28:54 +0545

    Spelling Changes

--- LAB 4/lab44.c
+++ LAB 4/lab44.c
@@ -11,6 +11,6 @@ int main() {
 	if ((num%7==0) && (num%11!=0))
 		printf("%d is divisible by 5 and 7 but not by 11.",num);
 	else
-		printf("%d is divisible by 5 but doesn't satisty the later condition .",num);
+		printf("%d is divisible by 5 but doesn't satisfy the later condition .",num);
 	return 0;
 }
