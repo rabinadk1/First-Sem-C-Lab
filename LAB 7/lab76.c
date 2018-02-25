68c6bf28a9602fdd0ff027de5e0665691f33fb64
Maverick <074bct529.rabin@pcampus.edu.np>
2018-02-25 22:44:19 +0545

    Added exclusions and changed lab76.c

--- LAB 7/lab76.c
+++ LAB 7/lab76.c
@@ -2,28 +2,20 @@
 int main()
 {
 	char a[10];
-	int i,len,c=1;
+	int len,j,c=1;
 	printf("Input string: ");
 	scanf("%s",a);
-	for (i=0;i<10;i++)
+	for (len=0;len<10 && a[len]=='\0';len++)
+	for (j=0;j<(len/2);j++)
 	{
-		if (a[i]=='\0')
-		{
-			len=i-1;
-			break;
-		}
-	}
-
-	for (i=0;i<(len/2);i++)
-	{
-		if (a[i]!=a[len-i])
+		if (a[j]!=a[len-j-1])
 		{
 			c=0;
 			break;
 		}
 	}
 	if (c)
-		printf("%s is palindrome.",a);
+		printf("%s is a palindrome.",a);
 	else
 		printf("%s isn't a palindrome.",a);
 	return 0;
