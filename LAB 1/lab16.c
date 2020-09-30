0eef1590ca1f517a5223fc86eb887a0b1ffbc465
BHIM <58538172+bhim5@users.noreply.github.com>
2020-09-30 20:21:05 +0545

    Update lab16.c

--- LAB 1/lab16.c
+++ LAB 1/lab16.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 int main () {
 	int P=4000,T=2;
-	float R=5.5,I;
-	I=P*T*R/100;
-	printf("The simple interest of Principal Rs.%d in time %d years with rate %.2f p.a is Rs.%.2f.",P,T,R,I);
+	float R=5.5,SimpleInterest;
+	SimpleInterest=P*T*R/100;
+	printf("The simple interest of Principal Rs.%d in time %d years with rate %.2f p.a is Rs.%.2f.",P,T,R,SimpleInterest);
 	return 0;
 }
